0759c304a94f77550624a11cad1f01e50ad95a61
yosuke0102 <95847760+yosuke0102@users.noreply.github.com>
2021-12-09 17:18:24 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+  el:"#app",
+  data: {
+    message: "写真を撮影しました",
+    seen: false
+  },
+  methods: {
+    move() {
+      const se = new Audio("D700シャッター音.mp3")
+      se.play()
+      this.seen = true
+      setTimeout(() => {
+        this.message = "写真を送信中..."
+      }, 1000)
+      setTimeout(() => {
+        this.message = "IPアドレス取得中..."
+      }, 3000)
+      setTimeout(() => {
+        this.message = "IPアドレスから住所特定中..."
+      }, 4000)
+      setTimeout(() => {
+        this.message = "情報送信中..."
+      }, 5000)
+      setTimeout(() => {
+        location.href = "invoic.html"
+      }, 10000)
+    }
+  }
+})
